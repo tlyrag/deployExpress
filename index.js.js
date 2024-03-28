06dbd10a49dc28a367c3fd36e7e6381bf727abe0
Lyra Ganem <lyraganemt@student.douglascollege.ca>
2024-03-28 16:42:40 -0700

    adding express files

---
+++ index.js.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html")
+});
+app.post("/", function (req, res) {
+    console.log(req.body)
+    var num1 = Number(req.body.num1); 
+    var num2 = Number(req.body.num2); 
+    var result = num1 + num2; 
+    res.send("the result of this calculator is " + result);
+});
+
+app.listen(3000, function () { 
+    console.log("server started on port 3000"); });
